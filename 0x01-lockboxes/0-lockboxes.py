1d58dcbc5a70eb47c63acfb51b02f61e9f0efea1
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-02-01 08:18:25 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
+    """ FUnction to unlock boxes"""
     seen = set()
     seen.add(0)
     stack = [0]
