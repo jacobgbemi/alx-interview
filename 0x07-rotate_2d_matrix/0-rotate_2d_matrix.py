dc91035e1209fcc34695b42befe58fa41e16b47e
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-03-14 10:34:12 +0100

    solution to task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python3
+"""2D matrix rotation module.
+"""
+
+
 def rotate_2d_matrix(matrix):
+	"""Rotates an m by n 2D matrix in place.
+    """
+	if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
     n = len(matrix)
     # Transpose the matrix
     for i in range(n):
