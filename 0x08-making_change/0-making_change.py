1cf238e9a6087d1e14f447364173158003798613
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-03-21 12:44:38 +0100

    gitignore

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,15 +7,30 @@ def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     amount total when given a pile of coins of different values.
     """
-    if total <= 0:
-        return 0
-
+    # Initialize a list to store the minimum number of coins
+    # required for each value from 0 to total
     dp = [float('inf')] * (total + 1)
+
+    # Base case: 0 coins are needed to make up a total of 0
     dp[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    # Iterate over all possible values from 1 to total
+    for i in range(1, total+1):
+        # Try every coin in the list that is smaller than or
+        # equal to the current value
+        for j in range(len(coins)):
+            if coins[j] <= i:
+                # Compute the minimum number of coins required for
+                # the current value
+                # by taking the minimum of the number of coins required
+                # for the current value
+                # minus the value of the current coin, plus 1
+                # (for the current coin itself)
+                dp[i] = min(dp[i], dp[i-coins[j]] + 1)
 
-    return dp[total] if dp[total] < float('inf') else -1
+    # If the final value in the dp table is still infinity, it means
+    # the total cannot be made up by any number of coins
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
